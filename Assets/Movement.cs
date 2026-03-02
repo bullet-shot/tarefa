abcccd09d919e454c0fcfcecdb844c68d647239e
Bullet <felipemoralessenai25@gmail.com>
2026-03-02 11:53:38 -0300

    movimento
    só mudei umas texturas e programei o movimento

---
+++ Assets/Movement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    int numero = 1;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+         if(Input.GetKey("w"))
+        {
+            transform.Translate(0,0.05f,0);
+        }
+          
+         if(Input.GetKey("a"))
+        {
+            transform.Translate(-0.05f,0,0);
+        }
+          
+        if(Input.GetKey("d"))
+        {
+            transform.Translate(0.05f,0,0);
+        }
+
+
+        
+        Physics.CheckSphere(transform.position,1.2404f);
+    }
+}
